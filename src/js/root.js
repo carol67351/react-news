e58041ea7d11b09108c1eadca1b3865f262d7711
carol67351 <wangyao3598@163.com>
2017-07-25 09:10:46 +0800

    news
    news

---
+++ src/js/root.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router,Route,hashHistory} from 'react-router';
+import {Button} from 'antd';
+import PCIndex from './components/pc_index';
+import PCNewsDetails from './components/pc_news_details';
+import PCUserCenter from './components/pc_usercenter';
+import MobileNewsDetails from './components/mobile_news_details';
+import MobileIndex from './components/mobile_index';
+import MobileUserCenter from './components/mobile_usercenter';
+import 'antd/dist/antd.css';
+import MediaQuery from 'react-responsive';
+export default class Root extends React.Component{
+  render(){
+    return (
+      //这里替换了之前的 Index，变成了程序的入口
+      <div>
+      <MediaQuery query='(min-device-width:1224px)'>
+        <Router history={hashHistory}>
+         <Route path="/" component={PCIndex}></Route>
+         <Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+         <Route path="/usercenter" component={PCUserCenter}></Route>
+        </Router>
+      </MediaQuery>
+      <MediaQuery query='(max-device-width:1224px)'>
+         <Router history={hashHistory}>
+         <Route path="/" component={MobileIndex}></Route>
+         <Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
+         <Route path="/usercenter" component={MobileUserCenter}></Route>
+        </Router>
+      </MediaQuery>
+      </div>
+    );  
+  };
+} 
+
+ReactDOM.render(<Root/>, document.getElementById('mainConeainer'));
